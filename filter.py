82cc26f9b335e9ed25f77f64a6dd8afb6785d37a
Elena_Serebro <savikovaelena2002@gmail.com>
2021-11-14 10:31:29 +0500

    Task 1. 0.2

--- filter.py
+++ filter.py
@@ -5,32 +5,28 @@ arr = np.array(img)
 #python filter.py
 
 mosaic_size = 5
-gradation_gray = 5
-
-
-np.clip(arr, 0, 255)#обработка  возможного переполнения???
-
+number_of_shades = 10
 height = len(arr)
 width = len(arr[1])
-print(height, width)
 
 i = 0
-while i < height - 1: #полосы справа и снизу обрабатываются корректно, если 11 заменить на 1
+while i < height - mosaic_size + 1:
     j = 0
-    while j < width - 1:
-        gray = 0
+    while j < width - mosaic_size + 1:
+        average_value = 0
         for n in range(i, i + mosaic_size):
             for n1 in range(j, j + mosaic_size):
-                R = arr[n][n1][0] #место, где запутался с именами переменных
+                R = arr[n][n1][0]
                 G = arr[n][n1][1]
                 B = arr[n][n1][2]
-                gray += (R + G + B) / 3 #при расчетах среднего необходимо поделить на 3
-        gray = int(gray // 10)
+                average_value += (R + G + B) / 3
+        average_value = int(average_value // 10)
+        conversion_factor = 255 / (number_of_shades - 1) # conversion_factor позволяет контролировать колличество оттенков серого
         for n in range(i, i + mosaic_size):
             for n1 in range(j, j + mosaic_size):
-                arr[n][n1][0] = int(gray // gradation_gray) * gradation_gray  #компоненты серого делим на три
-                arr[n][n1][1] = int(gray // gradation_gray) * gradation_gray
-                arr[n][n1][2] = int(gray // gradation_gray) * gradation_gray
+                arr[n][n1][0] = int(average_value / conversion_factor + 0.5) * conversion_factor  #тут была ошибка при преобразовании чисел с плавающей запятой
+                arr[n][n1][1] = int(average_value / conversion_factor + 0.5) * conversion_factor
+                arr[n][n1][2] = int(average_value / conversion_factor + 0.5) * conversion_factor
         j = j + mosaic_size
     i = i + mosaic_size
 
