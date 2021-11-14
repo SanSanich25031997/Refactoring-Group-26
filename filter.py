21c0014f71c7099ad2b582962d06401200e5279c
Elena_Serebro <savikovaelena2002@gmail.com>
2021-11-14 09:16:50 +0500

    Task 1. 0.1

--- filter.py
+++ filter.py
@@ -2,27 +2,37 @@ from PIL import Image
 import numpy as np
 img = Image.open("img2.jpg")
 arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
+#python filter.py
+
+mosaic_size = 5
+gradation_gray = 5
+
+
+np.clip(arr, 0, 255)#обработка  возможного переполнения???
+
+height = len(arr)
+width = len(arr[1])
+print(height, width)
+
 i = 0
-while i < a - 11:
+while i < height - 1: #полосы справа и снизу обрабатываются корректно, если 11 заменить на 1
     j = 0
-    while j < a1 - 11:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
+    while j < width - 1:
+        gray = 0
+        for n in range(i, i + mosaic_size):
+            for n1 in range(j, j + mosaic_size):
+                R = arr[n][n1][0] #место, где запутался с именами переменных
+                G = arr[n][n1][1]
+                B = arr[n][n1][2]
+                gray += (R + G + B) / 3 #при расчетах среднего необходимо поделить на 3
+        gray = int(gray // 10)
+        for n in range(i, i + mosaic_size):
+            for n1 in range(j, j + mosaic_size):
+                arr[n][n1][0] = int(gray // gradation_gray) * gradation_gray  #компоненты серого делим на три
+                arr[n][n1][1] = int(gray // gradation_gray) * gradation_gray
+                arr[n][n1][2] = int(gray // gradation_gray) * gradation_gray
+        j = j + mosaic_size
+    i = i + mosaic_size
+
 res = Image.fromarray(arr)
 res.save('res.jpg')
