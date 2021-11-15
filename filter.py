fe102e6c880835ac8510dee5c5b6c0ba5f5d9686
Elena_Serebro <savikovaelena2002@gmail.com>
2021-11-15 14:23:45 +0500

    Task 2

--- filter.py
+++ filter.py
@@ -1,34 +1,43 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
 #python filter.py
 
-mosaic_size = 5
-number_of_shades = 10
+def count_average_value(col, row):
+    average_value = 0
+
+    for i in range(col, col + mosaic_size):
+        for j in range(row, row + mosaic_size):
+            R = arr[i][j][0]
+            G = arr[i][j][1]
+            B = arr[i][j][2]
+            average_value += (R + G + B) / 3
+
+    return int(average_value // 10) if mosaic_size < 10 else int(average_value // 100)
+
+def paint_mosaic(col, row):
+    conversion_factor = 255 / (number_of_shades - 1)
+
+    for i in range(col, col + mosaic_size):
+        for j in range(row, row + mosaic_size):
+            arr[i][j][0] = int(average_value / conversion_factor + 0.5) * conversion_factor
+            arr[i][j][1] = int(average_value / conversion_factor + 0.5) * conversion_factor
+            arr[i][j][2] = int(average_value / conversion_factor + 0.5) * conversion_factor
+
+img = Image.open("img2.jpg")
+arr = np.array(img)
+mosaic_size = int(input('Введите размер мозайки: '))
+number_of_shades = int(input('Введите градацию серого: '))
 height = len(arr)
 width = len(arr[1])
 
-i = 0
-while i < height - mosaic_size + 1:
-    j = 0
-    while j < width - mosaic_size + 1:
-        average_value = 0
-        for n in range(i, i + mosaic_size):
-            for n1 in range(j, j + mosaic_size):
-                R = arr[n][n1][0]
-                G = arr[n][n1][1]
-                B = arr[n][n1][2]
-                average_value += (R + G + B) / 3
-        average_value = int(average_value // 10)
-        conversion_factor = 255 / (number_of_shades - 1) # conversion_factor позволяет контролировать колличество оттенков серого
-        for n in range(i, i + mosaic_size):
-            for n1 in range(j, j + mosaic_size):
-                arr[n][n1][0] = int(average_value / conversion_factor + 0.5) * conversion_factor  #тут была ошибка при преобразовании чисел с плавающей запятой
-                arr[n][n1][1] = int(average_value / conversion_factor + 0.5) * conversion_factor
-                arr[n][n1][2] = int(average_value / conversion_factor + 0.5) * conversion_factor
-        j = j + mosaic_size
-    i = i + mosaic_size
+col = 0
+while col < height - mosaic_size + 1:
+    row = 0
+    while row < width - mosaic_size + 1:
+        average_value = count_average_value(col, row)
+        paint_mosaic(col, row)
+        row = row + mosaic_size
+    col = col + mosaic_size
 
 res = Image.fromarray(arr)
 res.save('res.jpg')
