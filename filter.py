d829cb3bbc181d7ba3595530879fc3602b1babff
debebantur <stepan.filippov@yandex.ru>
2021-11-16 16:18:56 +0500

    2task

--- filter.py
+++ filter.py
@@ -1,26 +1,39 @@
 from PIL import Image
 import numpy as np
+
+
+def FindMedianSaturation(startX, startY, mosaicPix, imageMatrix):
+    medianSat = 0
+    for n in range(startX, startX + mosaicPix):
+        for n0 in range(startY, startY + mosaicPix):
+            redSat = imageMatrix[n][n0][0]
+            greenSat = imageMatrix[n][n0][1]
+            blueSat = imageMatrix[n][n0][2]
+            pixelSat = int(redSat) + int(greenSat) + int(blueSat)
+            medianSat += pixelSat/3
+    return int(medianSat // 100)
+
+
+def ApplyMedianSaturation(startX, startY, mosaicPix, medianSat,
+                          graySens, imageMatrix):
+    for n in range(startX, startX + mosaicPix):
+        for n1 in range(startY, startY + mosaicPix):
+            imageMatrix[n][n1][0] = int(medianSat // graySens) * graySens
+            imageMatrix[n][n1][1] = int(medianSat // graySens) * graySens
+            imageMatrix[n][n1][2] = int(medianSat // graySens) * graySens
+
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-for i in range(0, a -1,10):
-    for j in range(0, a1 -1,10):
-        s = 0
-        for n in range(i, i + 10):
-            for n0 in range(j, j + 10):
-                n1 = arr[n][n0][0]
-                n2 = arr[n][n0][1]
-                n3 = arr[n][n0][2]
-                M = int(n1) + int(n2) + int(n3)
-                s += M/3
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-res = Image.fromarray(arr)
-res.save('res5.jpg')
+imageMatrix = np.array(img)
+mosaicPix = 5
+graySens = 10
+height = len(imageMatrix)
+width = len(imageMatrix[1])
+for i in range(0, height - 1, mosaicPix):
+    for j in range(0, width - 1, mosaicPix):
+        medianSat = FindMedianSaturation(i, j, mosaicPix, imageMatrix)
+        ApplyMedianSaturation(i, j, mosaicPix, medianSat,
+                              graySens, imageMatrix)
+res = Image.fromarray(imageMatrix)
+res.save('res8.jpg')
 
 print('pilik pilik')
