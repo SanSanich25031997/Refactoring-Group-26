47331cae4fd063ead3e88585da157a3f9f17079c
debebantur <stepan.filippov@yandex.ru>
2021-11-16 17:08:39 +0500

    4task

--- filter.py
+++ filter.py
@@ -1,39 +1,37 @@
 from PIL import Image
 import numpy as np
 
-
-def FindMedianSaturation(startX, startY, mosaicPix, imageMatrix):
-    medianSat = 0
-    for n in range(startX, startX + mosaicPix):
-        for n0 in range(startY, startY + mosaicPix):
+def FindMedianSaturation(i,j,mosaicPartSide, imageMatrix):
+    medianSaturation = 0
+    for n in range(i, i + mosaicPartSide):
+        for n0 in range(j, j + mosaicPartSide):
             redSat = imageMatrix[n][n0][0]
             greenSat = imageMatrix[n][n0][1]
             blueSat = imageMatrix[n][n0][2]
             pixelSat = int(redSat) + int(greenSat) + int(blueSat)
-            medianSat += pixelSat/3
-    return int(medianSat // 100)
+            medianSaturation += pixelSat/3
+    return int(medianSaturation // 100)
 
+def ApplyMedianSaturation(i, j, mosaicPartSide, medianSaturation, graySensitivity, imageMatrix):
+    for n in range(i, i + mosaicPartSide):
+        for n1 in range(j, j + mosaicPartSide):
+            imageMatrix[n][n1][0] = int(medianSaturation // graySensitivity) * graySensitivity
+            imageMatrix[n][n1][1] = int(medianSaturation // graySensitivity) * graySensitivity
+            imageMatrix[n][n1][2] = int(medianSaturation // graySensitivity) * graySensitivity
 
-def ApplyMedianSaturation(startX, startY, mosaicPix, medianSat,
-                          graySens, imageMatrix):
-    for n in range(startX, startX + mosaicPix):
-        for n1 in range(startY, startY + mosaicPix):
-            imageMatrix[n][n1][0] = int(medianSat // graySens) * graySens
-            imageMatrix[n][n1][1] = int(medianSat // graySens) * graySens
-            imageMatrix[n][n1][2] = int(medianSat // graySens) * graySens
-
-img = Image.open("img2.jpg")
+print('введите полные названия файлов')
+imgadreses=input().split(' ')
+img = Image.open(imgadreses[0])
 imageMatrix = np.array(img)
-mosaicPix = 5
-graySens = 10
+mosaicPartSide = 5
+graySensitivity = 10
 height = len(imageMatrix)
 width = len(imageMatrix[1])
-for i in range(0, height - 1, mosaicPix):
-    for j in range(0, width - 1, mosaicPix):
-        medianSat = FindMedianSaturation(i, j, mosaicPix, imageMatrix)
-        ApplyMedianSaturation(i, j, mosaicPix, medianSat,
-                              graySens, imageMatrix)
+for i in range(0, height - 1, mosaicPartSide):
+    for j in range(0, width - 1, mosaicPartSide):
+        medianSaturation = FindMedianSaturation(i, j, mosaicPartSide, imageMatrix)
+        ApplyMedianSaturation(i, j, mosaicPartSide, medianSaturation, graySensitivity, imageMatrix)
 res = Image.fromarray(imageMatrix)
-res.save('res8.jpg')
+res.save(imgadreses[1])
 
 print('pilik pilik')
