63fd43a62d22e7d179672f6d6241542a63951266
Pavuuk <88192739+Pavuuk@users.noreply.github.com>
2021-11-17 17:21:57 +0500

    task 2

--- filter.py
+++ filter.py
@@ -1,28 +1,37 @@
 from PIL import Image
 import numpy as np
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 1:
-    j = 0
-    while j < a1 - 1:
-        s = 0
-        for n in range(i, i + 10):
-            for h in range(j, j + 10):
-                r = arr[n][h][0]
-                g = arr[n][h][1]
-                b = arr[n][h][2]
-                M = int(r) + int(g) +int(b)
-                s += M/3
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for h in range(j, j + 10):
-                arr[n][h][0] = int(s // 50) * 50
-                arr[n][h][1] = int(s // 50) * 50
-                arr[n][h][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
+arr_img = np.array(img)
+rows = len(arr_img)
+columns = len(arr_img[1])
+
+def set_clarity(size,number_column, number_row):
+    gray_tier = 0
+    for n in range(number_row, number_row + size):
+        for h in range(number_column, number_column + size):
+            r = arr_img[n][h][0]
+            g = arr_img[n][h][1]
+            b = arr_img[n][h][2]
+            sum_rgb = int(r) + int(g) +int(b)
+            gray_tier += sum_rgb/3
+    return int(gray_tier // (size**2))
+
+def set_grey_color(gray_tier, size, number_column, number_row, gradations):    
+    for n in range(number_row, number_row + size):
+        for h in range(number_column, number_column + size):
+            arr_img[n][h][0] = int(gray_tier // gradations) * gradations
+            arr_img[n][h][1] = int(gray_tier // gradations) * gradations
+            arr_img[n][h][2] = int(gray_tier // gradations) * gradations
+
+def create_mosaic(size, gradations):
+    number_row = 0
+    while number_row < rows :
+        number_column = 0
+        while number_column < columns :
+            set_grey_color(set_clarity(size,number_column, number_row), size, number_column, number_row, gradations )
+            number_column = number_column + size
+        number_row = number_row + size
+
+create_mosaic(2,10)
+res = Image.fromarray(arr_img)
 res.save('res.jpg')
