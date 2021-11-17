72f14a135a86275d80927a4dd6e331466e3af468
Alexey <cmango67@gmail.com>
2021-11-17 14:51:01 +0500

    Task 4 done

--- filter.py
+++ filter.py
@@ -21,20 +21,41 @@ def ApplyGrayscale(imgArray, brightness, i, j, cellHeight, cellWidth):
                     brightness // grayscaleValue) * grayscaleValue
 
 
-img = Image.open("img2.jpg")
+imgInName = input(
+    "Введите название входного изображения в формате \"imageName.jpg\":")
+
+if imgInName == '':
+    print('Пустые имена не разрешены!')
+    print('Выход из программы...')
+    quit()
+
+imgOutName = input(
+    "Введите название выходного изображения в формате \"resultName.jpg\":")
+
+if imgOutName == '':
+    print('Название выходного изображения будет заменено на \"resultImage.jpg\".')
+    imgOutName = 'resultImage.jpg'
+
+img = Image.open(imgInName)
 imgArray = np.array(img)
 height = len(imgArray)
 width = len(imgArray[0])
 
-cellHeight, cellWidth = 10, 10
-grayscaleSteps = 5
+cellHeight, cellWidth = 10, 10  # Высота и ширина мозаики (в пикселях)
+grayscaleSteps = 5  # Количество градаций серого
 grayscaleValue = int(255 / grayscaleSteps)
-
 
 for i in range(0, height - 1, cellHeight):
     for j in range(0, width - 1, cellWidth):
         brightness = GetCellBrightness(imgArray, i, j, cellHeight, cellWidth)
         ApplyGrayscale(imgArray, brightness, i, j, cellHeight, cellWidth)
-
 res = Image.fromarray(imgArray)
-res.save('res.jpg')
+
+try:
+    res.save(imgOutName)
+except BaseException:
+    print('Ошибка в записи файла, проверьте корректность названия выходного изображения.')
+    print('Выход из программы...')
+    quit()
+
+print('Дело сделано!')
